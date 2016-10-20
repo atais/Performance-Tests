c5205a0d11de82d45b9db01f6ad51f6e4c012391
Michał Siatkowski <atais.jr@gmail.com>
2016-10-20 20:44:35 +0200

    Future fun check

---
+++ src/test/scala/pl/msiatkowski/so/FutureCheck.scala
@@ -0,0 +1,45 @@
+package pl.msiatkowski.so
+
+import org.scalatest._
+
+import scala.collection.mutable
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class FutureCheck extends FlatSpec with Matchers {
+
+  "A simple" should "test" in {
+
+    val map = new mutable.HashMap[Int, Int]()
+
+    assert(map.isEmpty)
+
+    lazy val lazyF = Future {
+      map.put(1, 1)
+    }
+
+    assert(map.isEmpty)
+    Thread.sleep(1000)
+    assert(map.isEmpty)
+
+    for {
+      x <- lazyF
+    } yield x match {
+      case _ => assert(map.size == 1)
+    }
+
+    val nonLazyF = Future {
+      map.put(2, 2)
+    }
+
+    Thread.sleep(1000)
+    assert(map.size == 2)
+
+    for {
+      x <- nonLazyF
+    } yield x match {
+      case _ => assert(map.size == 2)
+    }
+  }
+
+}
